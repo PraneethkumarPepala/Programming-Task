c6112cb45d153c4731fd8a9be611bcbc71ea7a90
PraneethkumarPepala <praneethkumarpepala@gmail.com>
2025-09-24 20:19:28 +0530

    Create Program1.java

---
+++ Program1.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+
+
+public class Calculator {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        String op = sc.next();
+        switch (op.toLowerCase()) {
+        case "add":
+            System.out.println(a+b);
+            break;
+        case "subtract":
+        	System.out.println(a-b);
+        	break;
+        case "multiplie":
+        	System.out.println(a*b);
+        	break;
+        case "divide":
+        	System.out.println(a/b);
+        	break;
+        default:
+        	System.out.println("Invalid Operation");
+        }
+	
+	}
+}
